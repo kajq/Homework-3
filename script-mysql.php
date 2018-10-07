edb32bd2456221c111f0ab8fcad898251edb0b8f
Keilor Jimenez Quesada <keilorjimenez95@gmail.com>
2018-10-06 20:57:43 -0600

    Se realiza consulta de tabla users y se imprime en pantalla

--- script-mysql.php
+++ script-mysql.php
@@ -1,9 +1,14 @@
 <?php
-$mysqli = new MySQLi("localhost", "root","", "userdb");
+$mysqli = new MySQLi("localhost", "admin","123", "userdb");
 		if ($mysqli -> connect_errno) {
 			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
 				. ") " . $mysqli -> mysqli_connect_error());
 		} else {
-			echo "Conexion establecida";
+			echo "Conexión establecida..." . "\n";
+
+			$consulta=mysqli_query($mysqli,"SELECT * FROM users");
+			    while($arreglo=mysqli_fetch_array($consulta)){
+			    	echo $arreglo[0] . " | " . $arreglo[1] . " | " . $arreglo[2] . " | " . $arreglo[3] . "\n";
+				}
 		}
 ?>
