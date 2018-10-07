ba343c251bd44e8cb89316bea9503214405234a2
Keilor Jimenez Quesada <keilorjimenez95@gmail.com>
2018-10-06 21:05:42 -0600

    Se cambia codigo para que en ves de imprimir guarde en archivo de texto

--- script-mysql.php
+++ script-mysql.php
@@ -6,9 +6,14 @@ $mysqli = new MySQLi("localhost", "admin","123", "userdb");
 		} else {
 			echo "Conexión establecida..." . "\n";
 
+			$output = $argv[1];//nombre archivo csv
+			$newfile=fopen($output.'.txt','w');
+
 			$consulta=mysqli_query($mysqli,"SELECT * FROM users");
-			    while($arreglo=mysqli_fetch_array($consulta)){
-			    	echo $arreglo[0] . " | " . $arreglo[1] . " | " . $arreglo[2] . " | " . $arreglo[3] . "\n";
-				}
+		    while($arreglo=mysqli_fetch_array($consulta)){
+		    	fwrite($newfile, $arreglo[0] . " , " . $arreglo[1] . " , " . $arreglo[2] . " , " . $arreglo[3] . "\r\n");
+			}
+			echo "Tu archivo se ha guardado con el nombre: " . $output;
+			fclose($newfile); 	
 		}
 ?>
