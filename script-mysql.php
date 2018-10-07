821da8b5e408aa1acffb3b62125ea8b6374991ea
Keilor Jimenez Quesada <keilorjimenez95@gmail.com>
2018-10-06 21:21:51 -0600

    Se modifica para que capture los 6 parametros, además se hace una validación por si no se ingresan correctamente los parametros. Pruebas exitosas

--- script-mysql.php
+++ script-mysql.php
@@ -1,15 +1,25 @@
 <?php
-$mysqli = new MySQLi("localhost", "admin","123", "userdb");
+if ($argc < 7 )
+{
+    exit( "Este script requiere los siguientes parametros:
+         \t 1. user -> usuario de la base de datos (admin)
+		 2. pass -> contraseña para conectarse a la base de datos (123)
+		 3. database -> nombre de la base de datos (userdb)
+		 4. host -> nombre de host (localhost) 
+		 5. table -> la tabla de la base de datos para leer (users)
+		 6. output -> el archivo de salida (output.csv)" );
+}
+$mysqli = new MySQLi($argv[4], $argv[1],$argv[2], $argv[3]);
 		if ($mysqli -> connect_errno) {
 			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
 				. ") " . $mysqli -> mysqli_connect_error());
 		} else {
 			echo "Conexión establecida..." . "\n";
 
-			$output = $argv[1];//nombre archivo csv
-			$newfile=fopen($output.'.txt','w');
+			$output = $argv[6];//nombre archivo csv
+			$newfile=fopen($output.'.csv','w');
 
-			$consulta=mysqli_query($mysqli,"SELECT * FROM users");
+			$consulta=mysqli_query($mysqli,"SELECT * FROM $argv[5]");
 		    while($arreglo=mysqli_fetch_array($consulta)){
 		    	fwrite($newfile, $arreglo[0] . " , " . $arreglo[1] . " , " . $arreglo[2] . " , " . $arreglo[3] . "\r\n");
 			}
